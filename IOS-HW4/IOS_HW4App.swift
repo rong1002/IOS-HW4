abc12b91769cd4f8f205363b3aff2dc9f0a37c18
rong1002 <borong1002@gmail.com>
2020-11-19 15:28:14 +0800

    Initial Commit

---
+++ IOS-HW4/IOS_HW4App.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_HW4App.swift
+//  IOS-HW4
+//
+//  Created by Lin Bo Rong on 2020/11/19.
+//
+
+import SwiftUI
+
+@main
+struct IOS_HW4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
